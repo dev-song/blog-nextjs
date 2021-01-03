ec7d2a4c659062a3690e11b0d6a178ac7786158c
Sangsu Song <dvlprsong@gmail.com>
2021-01-03 18:54:50 +0900

    #10 Load parsed markdown data and display them on the application

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { pageTitle } from '../components/layout'
+
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -12,6 +25,22 @@ export default function Home() {
       <section className={utilStyles.headingMedium}>
         <p>Hi, I'm dev-song</p>
         <p>This is a sample blog built with Next.js</p>
+      </section>
+      <section className={`${utilStyles.headingMedium} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLarge}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <p className={utilStyles.listItemLine}>{title}</p>
+              <div>
+                <span className={utilStyles.listItemText}>{id}</span>
+                <span className={utilStyles.listItemText}>{date}</span>
+              </div>
+            </li>
+          ))
+
+          }
+        </ul>
       </section>
     </Layout>
   )
